243ead53217e33a116edc2deaaeaa757af1245a8
devlikebear <devlikebear@gmail.com>
2023-04-16 09:36:12 +0900

    feat: Pocketbase를 사용해 lottos API 구현

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,43 @@
+// main.go
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/devlikebear/aureum-cornu/pkg/lotto"
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-	lottoNumbers := lotto.GenerateLottoNumbers()
-	fmt.Println("생성된 로또 번호:", lottoNumbers)
+    app := pocketbase.New()
+
+    // 로또번호 조회하는 API 추가
+    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+        e.Router.AddRoute(echo.Route{
+            Method:  http.MethodGet,
+            Path:    "/api/lottos",
+            Handler: lottoHandler,
+        })
+        return nil  
+    })
+
+
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func lottoHandler(c echo.Context) error {
+    // 생성할 로또번호 개수를 파라미터로 받는다.
+    count := c.QueryParam("count")
+    n, _  := strconv.Atoi(count)
+
+    number := lotto.GenerateLottoNumbers(n)
+    return c.JSON(http.StatusOK, map[string]interface{}{
+        "numbers": number,
+    })
 }
